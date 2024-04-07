07c0a749bbce4b85ff4c78cf826e574eb9c15919
Ciont Antonia <ciontantonia@yahoo.com>
2024-04-07 13:05:24 +0300

    created node server using express.js

--- creativejoyServer/index.js
+++ creativejoyServer/index.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const app = express();
+const mysql2 = require("mysql2");
+
+const pool = mysql2.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password9898@",
+  database: "creativejoy",
+  port: 3306,
+  multipleStatements: true,
+});
+
+app.get("/", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send("Connection established");
+    }
+  });
+});
 const PORT = 5005;
 const server = app.listen(PORT, () => {
   console.log("App is running on port 5005");
